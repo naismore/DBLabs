885644dd19da8510aa937848d93e736e46f8b54c
naismore <124817694+naismore@users.noreply.github.com>
2024-10-16 16:25:31 +0300

    no message

--- dblw9/Models/Configurations/ItemsInStorageConfiguration.cs
+++ dblw9/Models/Configurations/ItemsInStorageConfiguration.cs
@@ -7,7 +7,11 @@ namespace dblw9.Models.Configurations
     {
         public void Configure(EntityTypeBuilder<ItemsInStorage> builder)
         {
-
+            builder.Property(iis => iis.Id).HasColumnName("id");
+            builder.HasKey(iis => iis.Id);
+            builder.Property(iis => iis.StorageId).HasColumnName("id_storage");
+            builder.Property(iis => iis.ItemId).HasColumnName("id_item");
+            builder.Property(iis => iis.ArrialDate).HasColumnType("datetime").IsRequired();
         }    
     }
 }
--- dblw9/Program.cs
+++ dblw9/Program.cs
@@ -1,26 +1,81 @@
-﻿namespace dblw9
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace dblw9
 {
     internal class Program
     {
+        const char SELECT_CHAR = '>';
+        public static string[] categoriesActions = {
+            "Товары",
+            "Склады",
+        };
+        public static string[] itemsActions = { 
+            "Найти товар по имени",
+            "Найти товар по ID",
+            "Переместить товар на другой склад",
+            "Удалить данные о товаре"
+        };
+        public static string[] storageActions =
+        {
+
+        };
         public static void Main(string[] args)
         {
-            using (MyDbContext db = new MyDbContext())
+            while (true)
             {
-                Supplier supplier1 = new Supplier { Name = "АО ММЗ", ContactPersonFirstName = "Аяз", ContactPersonLastName = "Ахметшин", PhoneNumber = "+7123412312", Adress = "ayazAhmetshin@nAts.eu"};
-
-                db.Add(supplier1);
-                db.SaveChanges();
-            }
-
-            using (MyDbContext db = new MyDbContext())
-            {
-                Supplier? supplier = db.Suppliers.FirstOrDefault();
-
-                if (supplier != null)
+                if (!TestConnectDB())
                 {
-                    Console.WriteLine($"{supplier.Id}. {supplier.Name}\nОтветственный: {supplier.ContactPerson}\nНомер телефона: {supplier.PhoneNumber}\nАдрес: {supplier.Adress}");
+                    Console.WriteLine("Ошибка: Подключение к базе данных отсутствует");
+                }
+                ShowMenu(categoriesActions);
+                var key = Console.ReadKey(true).Key;
+
+                switch(key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Введите название или ID товара: ");
+                        string? itemRawString = Console.ReadLine();
+                        if (itemRawString != null)
+                        {
+                            Console.WriteLine(GetItem(itemRawString));
+                        } 
+                        break;
                 }
             }
         }
+
+        public static bool TestConnectDB()
+        {
+            using (MyDbContext db = new())
+            {
+                return db.Database.CanConnect();
+            }  
+        }
+
+        public static void ShowMenu(string[] actions)
+        {
+            Console.WriteLine("Выберите действие: ");
+            for (int i = 0; i < actions.Length; i++)
+            {
+                Console.WriteLine($"{i}. {actions[i]}");
+            }
+            Console.WriteLine("Нажмите Esc для выхода");
+        }
+
+        public static string GetItem(string itemRawString)
+        {
+            int itemId;
+            if (int.TryParse(itemRawString, out itemId))
+            {
+                return GetItemById(id);
+            }
+            else
+            {
+                return GetItemByName(itemRawString);
+            }
+        }
+
+        
     }
 }
